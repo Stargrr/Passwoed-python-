d6272497894f8a963ff3070a9678d658b89d16eb
Stargrr <simantgamer04@gmail.com>
2024-10-28 12:12:35 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+input = input("Enter Password :")
+
+if(input == "255"):
+    print("Correct password ✅")
+else:
+    print("Rong Password please try again")
+ 
+if(input == "255"):
+    print("Now link is unlocked you can access it The link is https://aashabusinesscatalayst.top")
